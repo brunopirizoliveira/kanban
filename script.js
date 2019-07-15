d85739b03f387d176e7101c33b9f9cae5c171533
brunooliverdwl <bruno.pires@qualitor.com.br>
2019-07-15 17:08:45 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+Sortable.create(list_backlog_1, { group: 'shared', animation: 150, sort: false });
+
+Sortable.create(list_user_1, { group: 'shared', animation: 150, sort: false });
+Sortable.create(list_user_2, { group: 'shared', animation: 150, sort: false });
+Sortable.create(list_user_3, { group: 'shared', animation: 150, sort: false });
+Sortable.create(list_user_4, { group: 'shared', animation: 150, sort: false });
+Sortable.create(list_user_5, { group: 'shared', animation: 150, sort: false });
+Sortable.create(list_user_6, { group: 'shared', animation: 150, sort: false });
+Sortable.create(list_user_7, { group: 'shared', animation: 150, sort: false });
+Sortable.create(list_user_8, { group: 'shared', animation: 150, sort: false });
+
+//  $("div[id^='card_backlog_']").on("click", function() {
+//     $('#myModal').modal('show');
+//  });
+
+ $(".disparaCard").on("click", function() {
+    $('#myModal').modal('show');
+ });
